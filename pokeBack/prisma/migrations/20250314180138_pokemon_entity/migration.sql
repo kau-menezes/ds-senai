7bec7169bea0072f0e6d3b37b4a25e7d5fe1b064
MuryloSaladino <murylosaladino@gmail.com>
2025-03-19 21:23:55 -0300

    refact: name in pokemon

--- pokeBack/prisma/migrations/20250314180138_pokemon_entity/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE `Pokemon` (
-    `id` INTEGER NOT NULL,
-    `name` VARCHAR(191) NOT NULL,
-
-    UNIQUE INDEX `Pokemon_id_key`(`id`),
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
