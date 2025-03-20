e6fd25bd1413c2c05414777fe3142cfd70d0eb9d
MuryloSaladino <murylosaladino@gmail.com>
2025-03-19 21:25:08 -0300

    feat: middlewares for auth and errors

--- pokeBack/src/middlewares/handleerror.ts
+++ pokeBack/src/middlewares/handleerror.ts
@@ -1,17 +1,15 @@
-import { NextFunction, Request, Response } from "express";
-import AppError from "../errors/AppError.ts";
+import { NextFunction, Request, Response } from "express"
+import AppError from "../errors/AppError";
 
-export async function handleError(error: any, req: Request, res: Response, next: NextFunction) {
+export default function handleError(err:Error, _req:Request, res:Response, next:NextFunction) {
+    switch (err.constructor) {
+        
+        case AppError:
+            const appError = err as AppError;
+            return res.status(appError.statusCode).json({ message: err.message });
 
-    if(error instanceof AppError) {
-        const err = error as AppError;
-        return res.status(err.statusCode).json({
-            message: error.message
-        })
+        default:
+            if(process.env.NODE_ENV === "dev") console.error(err);
+            return res.status(500).json({ message: "Internal Server Error" });
     }
-
-    console.error(error)
-    return res.status(500).json({
-        message: "Server Internal Error nem a gente sabe oq rolou"
-    })
 }
--- pokeBack/src/middlewares/validate.middleware.ts
+++ pokeBack/src/middlewares/validate.middleware.ts
@@ -1,51 +1,30 @@
-import pkg, { JwtPayload } from 'jsonwebtoken';
 import { NextFunction, Request, Response } from "express";
-import AppError from '../errors/AppError.ts';
-const { verify } = pkg;
+import { verify } from "jsonwebtoken";
+import AppError from "../errors/AppError";
 
-interface IToken {
-    id?: string, 
-    name?: string
-}
-
-
-export async function validateToken(req: Request, res: Response, next: NextFunction) {
-
-    console.log("Token validation begin...")
+export default async function authenticate(req:Request, res:Response, next:NextFunction) {
     
-    let token = req.headers.authorization;
-
-    if (!token) {
-        return next(new AppError('No token provided', 401));
+    let authenticated = false;
+    const auth = req.headers.authorization;
+    
+    if(!auth) {
+        res.status(401).json({message:"unauthorized"}) 
+        return
     }
-
-    token = token.replace("Bearer ", "")
+    const [_bearer, token] = auth.split(" ");
 
     verify(
-        token, 
-        String(process.env.SECRET_KEY),
-
-
-        (err, decoded) => {
-
-            if (err) {
-                throw new AppError(err.message, 401);
+        token,
+        String(process.env.JWT_SECRET_KEY),
+        (err:any, decoded:any) => {
+            if(!err) {
+                authenticated = true;
             }
-
-
-            if (decoded) {
-
-                const payload = decoded as JwtPayload;
-
-                res.locals.userID = payload.id;
-                res.locals.userName = payload.userType;
-            } else {
-                
-                throw new AppError('Invalid token', 401);
-            }
+            res.locals.userId = decoded.userId
         }
-    );
-
-
-    next();
+    )
+    
+    if(authenticated) {
+        next()
+    }
 }
